55037f66fa31669c2d0cb2bf306227c25213cd86
Rylie Castell <rylie.castell@gmail.com>
2023-05-25 15:35:17 +0200

    add fetch function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,31 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      data.results.forEach((character) => {
+        createCharacterCard(
+          character.image,
+          character.name,
+          character.status,
+          character.species,
+          character.type,
+          character.episode.length
+        );
+      });
+      return data.results;
+    } else {
+      console.error("Bad Response");
+    }
+  } catch (error) {
+    console.error("An error occured");
+  }
+}
+
+/* imgsrc, name, status, species, occurences */
+fetchCharacters();
